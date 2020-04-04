db3036b5bcdcbe5f452075c477cfd5419c7072a9
qinxiangyang <qinxiangyang@xinhuamm.net>
2020-04-04 22:30:06 +0800

    web项目搭建

--- 03-webdemo/src/test/java/com/yueyang/webdemo/ApplicationTests.java
+++ 03-webdemo/src/test/java/com/yueyang/webdemo/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.yueyang.webdemo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
